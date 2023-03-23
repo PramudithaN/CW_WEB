b7d6f7cd5982944c9c75e9f02466863ad345def9
Pramuditha <pramudithanadun@gmail.com>
2023-03-23 13:59:39 +0530

    main sync v15

--- Splashscreen/splashscreen.js
+++ Splashscreen/splashscreen.js
@@ -24,3 +24,17 @@
 //         },2300)
 //     })
 // })
+
+let countdown = document.getElementById("countdown");
+let seconds = 4;
+let timer = setInterval(()=>{
+    seconds--;
+    countdown.innerHTML = seconds;
+    if(seconds== 0){
+
+        clearInterval(timer);
+        window.location.href = "../MainPage/Home.html";
+        // href="../MainPage/Home.html";
+    }
+    },1000);
+
